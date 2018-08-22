8ce2694bfcadef848565e85b0861c771d63d067d
Nando179439 <fernando_5341@hotmail.com>
2018-08-22 19:05:09 -0300

    Se modifica eventcardcreateinvitation segun bocetos

--- app/modules/shared/Event/EventCardCreateInvitation/EventCardCreateInvitation.js
+++ app/modules/shared/Event/EventCardCreateInvitation/EventCardCreateInvitation.js
@@ -1,53 +1,33 @@
 import React, { PureComponent } from 'react';
 import { View, Text, Image } from 'react-native';
-import { Card, Button as ButtonElements } from 'react-native-elements';
 import { connect } from "react-redux";
 import { Actions } from "react-native-router-flux";
 import styles from "./styles";
+import { getFormalDate } from "../../utils/date";
+import LocationButton from "../components/LocationButton";
 
 class EventCardCreateInvitation extends PureComponent {
 
-    state = {
-        toggleSeeMore: true
-    }
-
-    // onToggleSeeMore = () => {
-    //     this.setState({toggleSeeMore: !this.state.toggleSeeMore});
-    // }
-
     render() {
-        const { id, title, type, realizationDate, place, image, categories, description } = this.props.eventInvitation;
+        const { title, realizationDate, place, image } = this.props.eventInvitation;
 
-        return(
+        return (
             <View style={styles.container}>
-                <View style={{ flex: 1 }}>
+                <View>
                     <Image
                         style={styles.image}
-                        resizeMode="cover"
+                        resizeMode='cover'
                         source={{ uri: image }}
                     />
                 </View>
-                <View style={{ flex: 2, flexDirection: "column", alignItems: "flex-start" }}>
-                    <View>
-                        <Text style={styles.title}>{title}</Text>
-                    </View>
-                    {
-                        !!description && 
-                        <View style={{marginBottom: 5}}>
-                            <Text>
-                                {
-                                    // (description.length > 180 && this.state.toggleSeeMore)
-                                    (description.length > 180)
-                                    ? description.substring(0, 180) + '...'
-                                    : description
-                                }
-                            </Text>
-                        </View>
-                    }
+                <View style={{ flexDirection: 'column', justifyContent: 'center', marginHorizontal: 10 }}>
+                    <Text style={styles.title}>{title}</Text>
+                    <Text style={styles.realizationDate}>{getFormalDate(realizationDate)}</Text>
+                    <LocationButton place={place} />
                 </View>
             </View>
         )
     }
 }
 
-export default connect(null, { })(EventCardCreateInvitation);
+export default connect(null, {})(EventCardCreateInvitation);
