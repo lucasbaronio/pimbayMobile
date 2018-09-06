a15f0e453019b758d9689605019928d7e62535af
Nando179439 <fernando_5341@hotmail.com>
2018-09-06 20:42:47 -0300

    Se agregan componentes en pantalla, faltan estilos

--- app/modules/eventDetail/scenes/EventDetail.js
+++ app/modules/eventDetail/scenes/EventDetail.js
@@ -1,43 +1,56 @@
 import React, { Component } from 'react';
-import { View, ActivityIndicator, Text } from 'react-native';
+import { View, ActivityIndicator, Text, ScrollView, Image } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-
+import { MapView } from 'expo';
 import { actions as eventDetailActions } from "../index";
 const { getEvent } = eventDetailActions;
 
 import { connect } from 'react-redux';
 import styles from './styles';
-
+import { theme } from "../../index";
+const { fontSize, color, windowWidth } = theme;
 
 class EventDetail extends Component {
 
-    state = {
-        //isLoadingEvent: false,
-        //event: null
-    }
-
     componentWillMount() {
-        const { item } = this.props;
-        console.log("acaaaa");
         console.log(this.props);
-        //this.props.getEvent(eventId);
-    }
-
-    renderEventInvitation = () => {
-        //const { eventInvitation } = this.state;
-        return (
-            <View style={styles.eventContainer}>
-                <EventCardCreateInvitation eventInvitation={eventInvitation} />
-            </View>
-        );
     }
 
     render() {
-        //const { type } = this.props;
+        const { title, realizationDate, place, image, categories, description } = this.props.item;
         return (
-            <View >
-                <Text>Event detail</Text>
-            </View>
+            <ScrollView style={{ bakgroundColor: color.white }}>
+                <View>
+                    <Image source={{ uri: image }} style={{ height: 200, width: windowWidth, alignSelf: 'stretch' }}></Image>
+                </View>
+                <View style={{ bakgroundColor: color.white }}>
+                    <Text style={{ bakgroundColor: color.white }}>{title}</Text>
+                    <Text style={{ bakgroundColor: color.white }}>{realizationDate}</Text>
+                    <Text style={{ bakgroundColor: color.white }}>{place}</Text>
+                </View>
+                <View style={{ height: 1, bakgroundColor: color.grey }} />
+                <View>
+                    <Text>Detalles</Text>
+                    <Text>{description}</Text>
+                </View>
+                <View style={{ height: 1, bakgroundColor: color.grey }} />
+                <View>
+                    <Text>Mapa</Text>
+                    <Text>{place}</Text>
+                    <View pointerEvents="none">
+                        <MapView
+
+                            style={{ margin: 20, height: 250, width: (windowWidth - 40) }}
+                            initialRegion={{
+                                latitude: 37.78825,
+                                longitude: -122.4324,
+                                latitudeDelta: 0.0922,
+                                longitudeDelta: 0.0421,
+                            }}
+                        />
+                    </View>
+                </View>
+            </ScrollView>
         );
     }
 }
