9b30117fddf158e422581520fe0d98c999cceecb
Nando179439 <fernando_5341@hotmail.com>
2018-09-08 11:33:53 -0300

    Se hace refactor de EventDetail

--- app/modules/eventDetail/constants.js
+++
@@ -1,5 +0,0 @@
-import { 
-    API_EVENT_BY_ID as apiGetEventById
-} from "../../config/constants";
-
-export const API_GET_EVENT_BY_ID = apiGetEventById;
--- app/redux/rootReducer.js
+++ app/redux/rootReducer.js
@@ -3,9 +3,8 @@ import { combineReducers } from 'redux';
 import { reducer as authReducer } from "../modules/auth"
 import { reducer as timelineReducer } from "../modules/timeline"
 import { reducer as invitationsReducer } from "../modules/myInvitations"
-import { reducer as eventDetailReducer } from "../modules/eventDetail"
 
 // Combine all the reducers
-const rootReducer = combineReducers({ authReducer, timelineReducer, invitationsReducer, eventDetailReducer });
+const rootReducer = combineReducers({ authReducer, timelineReducer, invitationsReducer });
 
 export default rootReducer;
